404b84f68077bde3997dd360d04ef8bd46087af9
Sin Wildy <sw.wildy@gmail.com>
2020-11-08 16:48:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,6 +17,7 @@ def get_japanese_emoticon(path, emoticons)
       return value[:japanese]
     end
   end 
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, emoticons)
