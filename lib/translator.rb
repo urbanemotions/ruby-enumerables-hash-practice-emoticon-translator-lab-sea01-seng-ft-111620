311b7dc39d7bbf44e7e5f4fee49e9b8e821b5ee4
Sin Wildy <sw.wildy@gmail.com>
2020-11-08 15:56:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,10 @@
 require 'pry' 
 require 'yaml'
-emoticons = YAML.load_file('./lib/emoticons.yml')
+#emoticons = YAML.load_file('/lib/emoticons.yml')
 
 def load_library
   # code goes here
-  #emoticons = YAML.load_file('./lib/emoticons.yml')
+  emoticons = YAML.load_file('/lib/emoticons.yml')
 end
 
 def get_japanese_emoticon
