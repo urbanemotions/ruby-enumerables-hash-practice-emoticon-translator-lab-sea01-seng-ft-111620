568959de4a133edb989ffdb8018ba68d365bb099
Sin Wildy <sw.wildy@gmail.com>
2020-11-08 16:33:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,10 +5,10 @@ def load_library(path)
   final_hash = {}
   YAML.load_file(path).each do |key, value|
     final_hash[key] = {}
-    final_hash[english] = value[0]
-    final_hash[japanese] = value[1]
+    final_hash[key][:english] = value[0]
+    final_hash[key][:japanese] = value[1]
   end 
-  # binding.pry
+  final_hash
 end
 
 def get_japanese_emoticon
